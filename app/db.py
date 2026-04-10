eb0ac9d53aa46956c0f733fda96b26b0253dfa20
URANIUM <165466934+DevURANIUM@users.noreply.github.com>
2026-04-10 16:59:00 +0330

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,19 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, DeclarativeBase
-
-# DATABASE_URL = "sqlite:///./pastes.db"
-
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={"check_same_thread": False},
-# )
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# class Base(DeclarativeBase):
-#     pass
-
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
